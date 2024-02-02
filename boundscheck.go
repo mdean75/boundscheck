ac90222336d6411253cdb9b1386af17cfdd693a5
Michael DeAngelo <deangelomp@gmail.com>
2024-02-02 06:04:59 -0600

    handled many cases that caused panic due to nil pointer and/or failed type assertion

--- boundscheck.go
+++ boundscheck.go
@@ -2,6 +2,7 @@ package linters
 
 import (
 	"go/ast"
+	"go/token"
 	"golang.org/x/tools/go/analysis"
 	"strconv"
 )
@@ -18,60 +19,12 @@ func run(pass *analysis.Pass) (interface{}, error) {
 	//errMsg := ""
 
 	for _, file := range pass.Files {
-		//ast.Inspect(file, func(n ast.Node) bool {
-		//	if comment, ok := n.(*ast.Comment); ok {
-		//		if strings.HasPrefix(comment.Text, "// TODO:") || strings.HasPrefix(comment.Text, "// TODO():") {
-		//			pass.Report(analysis.Diagnostic{
-		//				Pos:            comment.Pos(),
-		//				End:            0,
-		//				Category:       "todo",
-		//				Message:        "TODO comment has no author",
-		//				SuggestedFixes: nil,
-		//			})
-		//		}
-		//	}
-		//
-		//	return true
-		//})
 
 		ast.Inspect(file, func(n ast.Node) bool {
 
 			switch x := n.(type) {
 			case *ast.IfStmt:
-				//a := n.(*ast.IfStmt)
-				//b := a.Cond.(*ast.BinaryExpr)
-				//left := b.X.(*ast.CallExpr)
-				//
-				//var rightValue string
-				//
-				//right, ok := b.Y.(*ast.BasicLit)
-				//if ok {
-				//	rightValue = right.Value
-				//} else {
-				//	//rightValue = b.Y.(*ast.Ident).Obj.Decl.(*ast.AssignStmt).Rhs[0].(*ast.BasicLit).Value
-				//	//if ident, ok := b.Y.(*ast.Ident); ok {
-				//	//	if assign, ok := ident.Obj.Decl.(*ast.AssignStmt); ok {
-				//	//		if lit, ok := assign.Rhs[0].(*ast.BasicLit); ok {
-				//	//			rightValue = lit.Value
-				//	//		}
-				//	//	}
-				//	//}
-				//	ident, ok := b.Y.(*ast.Ident)
-				//	if !ok {
-				//		//return false
-				//
-				//	}
-				//	assign, ok := ident.Obj.Decl.(*ast.AssignStmt)
-				//	if !ok {
-				//		//return false
-				//	}
-				//	lit, ok := assign.Rhs[0].(*ast.BasicLit)
-				//	if !ok {
-				//		//return false
-				//	}
-				//	rightValue = lit.Value
-				//	lenCheckMap[left.Args[0].(*ast.Ident).Name] = rightValue
-				//}
+
 				a := n.(*ast.IfStmt)
 				b, ok := a.Cond.(*ast.BinaryExpr)
 				if !ok {
@@ -89,7 +42,25 @@ func run(pass *analysis.Pass) (interface{}, error) {
 				if ok {
 					rightValue = right.Value
 				} else {
-					rightValue = b.Y.(*ast.Ident).Obj.Decl.(*ast.AssignStmt).Rhs[0].(*ast.BasicLit).Value
+					rightIdent, ok := b.Y.(*ast.Ident)
+					if !ok {
+						return false
+					}
+					if rightIdent == nil {
+						return false
+					}
+					if rightIdent.Obj == nil {
+						return false
+					}
+					rightAssign, ok := rightIdent.Obj.Decl.(*ast.AssignStmt)
+					if !ok {
+						return false
+					}
+					rightLit, ok := rightAssign.Rhs[0].(*ast.BasicLit) //.Value
+					if !ok {
+						return false
+					}
+					rightValue = rightLit.Value
 				}
 
 				ident, ok := left.Args[0].(*ast.Ident)
@@ -99,23 +70,90 @@ func run(pass *analysis.Pass) (interface{}, error) {
 				lenCheckMap[ident.Name] = rightValue
 
 			case *ast.IndexExpr:
+
+				if a, ok := x.Index.(*ast.BinaryExpr); ok {
+					if aa, ok := a.X.(*ast.CallExpr); ok {
+						if funname, ok := aa.Fun.(*ast.Ident); ok {
+							if funname.Name == "len" {
+								return false
+							}
+
+						}
+					}
+				}
+
 				ident, ok := x.X.(*ast.Ident)
 				if !ok {
 					return false
 				}
+
 				name := ident.Name
 				var value string
 
+				if ident.Obj == nil {
+					return false
+				}
+				if ident.Obj.Decl == nil {
+					return false
+				}
+				if _, ok := ident.Obj.Decl.(*ast.Field); ok {
+					return false
+				}
+				if _, ok := ident.Obj.Decl.(*ast.ValueSpec); ok {
+					return false
+				}
+				if a, ok := ident.Obj.Decl.(*ast.AssignStmt).Rhs[0].(*ast.CallExpr); ok {
+					if _, ok := a.Args[0].(*ast.MapType); ok {
+						return false
+					}
+
+				}
 				switch i := x.Index.(type) {
 				case *ast.BasicLit:
+					// TODO: NOT QUITE RIGHT, NEED TO CHECK THE VARIABLE TYPE IS NOT MAP
+					m, ok := x.X.(*ast.Ident).Obj.Decl.(*ast.AssignStmt).Rhs[0].(*ast.CompositeLit) //.Args[0].(*ast.MapType); !ok { //.Decl.(*ast.AssignStmt).Rhs[0].(*ast.CallExpr).Args[0].(*ast.MapType); !ok {
+					if !ok {
+						return false
+					}
+					_, ok = m.Type.(*ast.ArrayType)
+					if !ok {
+						return false
+					}
+
+					//_, ok = m.Args[0].(*ast.MapType)
+					//if !ok {
+					//	return false
+					//}
+
+					//x.X.Obj.Decl.(*ast.AssignStmt).Rhs[0].(*ast.CallExpr).Args[0] == maptype
+					if i.Kind != token.INT {
+						return false
+					}
 					value = i.Value
 
 				case *ast.Ident:
+					if i.Obj == nil {
+						return false
+					}
+					if i.Obj.Decl == nil {
+						return false
+					}
+
+					if _, ok := i.Obj.Decl.(*ast.Field); ok {
+						return false
+					}
 					a, ok := i.Obj.Decl.(*ast.AssignStmt).Rhs[0].(*ast.BasicLit)
 					if !ok {
 						return false
 					}
 					value = a.Value
+
+				case *ast.CallExpr:
+					_, ok := i.Args[0].(*ast.MapType)
+					if ok {
+						return false
+					}
+
 				}
 
 				// lookup bounds check
@@ -141,6 +179,7 @@ func run(pass *analysis.Pass) (interface{}, error) {
 
 			return true
 		})
+		//fmt.Println(lenCheckMap)
 	}
 
 	return nil, nil
